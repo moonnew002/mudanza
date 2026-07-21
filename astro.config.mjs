d25b239e696e041bff819e4c4f457d87fdd701e3
Erick Luna <mauriciolunadejesus457@gmail.com>
2026-07-21 11:56:25 -0600

    Configuración correcta para despliegue en github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://erickluna.dev',
-  base: '/nombre-de-tu-repositorio',
+    site: 'https://moonnew002.github.io',
+    base: '/mudanza',
 });
